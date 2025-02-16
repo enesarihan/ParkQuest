95517e599f51028a67fd850faf86f6c40da27c52
enesarihan <enessarihan35@icloud.com>
2025-02-16 21:04:28 +0300

    error handling added.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const ParkingLot = require("./models/parkingLot");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
+const Joi = require("joi");
 const port = 3000;
+const ParkingLot = require("./models/parkingLot");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
+const { parkingLotSchema } = require("./schemas");
 
 mongoose.connect("mongodb://localhost:27017/park-quest");
 
@@ -23,52 +27,94 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+const validateParkingLot = (req, res, next) => {
+  const { error } = parkingLotSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
 app.get("/", (req, res) => {
   res.render("home");
 });
-app.get("/parkingLots", async (req, res) => {
-  const parkingLots = await ParkingLot.find({});
-  res.render("parkingLots/index", { parkingLots });
-});
+app.get(
+  "/parkingLots",
+  catchAsync(async (req, res) => {
+    const parkingLots = await ParkingLot.find({});
+    res.render("parkingLots/index", { parkingLots });
+  })
+);
 
 app.get("/parkingLots/new", (req, res) => {
   res.render("parkingLots/new");
 });
-app.post("/parkingLots", async (req, res) => {
-  const parkingLot = new ParkingLot(req.body.parkingLot);
-  await parkingLot.save();
-  res.redirect(`/parkingLots/${parkingLot._id}`);
+app.post(
+  "/parkingLots",
+  validateParkingLot,
+  catchAsync(async (req, res) => {
+    // if (!req.body.parkingLot)
+    //   throw new ExpressError("Invalid Parking lot Data", 400);
+    const parkingLot = new ParkingLot(req.body.parkingLot);
+    await parkingLot.save();
+    res.redirect(`/parkingLots/${parkingLot._id}`);
+  })
+);
+
+app.get(
+  "/parkingLots/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const parkingLot = await ParkingLot.findById(id);
+    res.render("parkingLots/show", { parkingLot });
+  })
+);
+
+app.get(
+  "/parkingLots/:id/edit",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const parkingLot = await ParkingLot.findById(id);
+    res.render("parkingLots/edit", { parkingLot });
+  })
+);
+app.put(
+  "/parkingLots/:id",
+  validateParkingLot,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const parkingLot = await ParkingLot.findByIdAndUpdate(
+      id,
+      { ...req.body.parkingLot },
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    await parkingLot.save();
+    res.redirect(`/parkingLots/${id}`);
+  })
+);
+
+app.delete(
+  "/parkingLots/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await ParkingLot.findByIdAndDelete(id);
+    res.redirect("/parkingLots");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found ", 404));
 });
 
-app.get("/parkingLots/:id", async (req, res) => {
-  const { id } = req.params;
-  const parkingLot = await ParkingLot.findById(id);
-  res.render("parkingLots/show", { parkingLot });
-});
-
-app.get("/parkingLots/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const parkingLot = await ParkingLot.findById(id);
-  res.render("parkingLots/edit", { parkingLot });
-});
-app.put("/parkingLots/:id", async (req, res) => {
-  const { id } = req.params;
-  const parkingLot = await ParkingLot.findByIdAndUpdate(
-    id,
-    { ...req.body.parkingLot },
-    {
-      runValidators: true,
-      new: true,
-    }
-  );
-  await parkingLot.save();
-  res.redirect(`/parkingLots/${id}`);
-});
-
-app.delete("/parkingLots/:id", async (req, res) => {
-  const { id } = req.params;
-  await ParkingLot.findByIdAndDelete(id);
-  res.redirect("/parkingLots");
+app.use((err, req, res, next) => {
+  const { status = 500 } = err;
+  if (!err.message) err.message = "Oh No, We Caught An Error!";
+  res.status(status).render("error", { err });
 });
 
 app.listen(port, () => {
---
+++ schemas.js
@@ -0,0 +1,13 @@
+const joi = require("joi");
+
+module.exports.parkingLotSchema = joi.object({
+  parkingLot: joi
+    .object({
+      title: joi.string().required(),
+      price: joi.number().required().min(0),
+      image: joi.string().required(),
+      location: joi.string().required(),
+      description: joi.string().required(),
+    })
+    .required(),
+});
