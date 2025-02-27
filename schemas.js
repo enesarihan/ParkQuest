d388d691a66f775f7e79240835ff06c8e4110925
enesarihan <enessarihan35@icloud.com>
2025-02-27 23:20:30 +0300

    Security features added

--- schemas.js
+++ schemas.js
@@ -1,13 +1,37 @@
-const joi = require("joi");
+const BaseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value)
+          return helpers.error("string.escapeHTML", { value });
+        return clean;
+      },
+    },
+  },
+});
+
+const joi = BaseJoi.extend(extension);
 
 module.exports.parkingLotSchema = joi.object({
   parkingLot: joi
     .object({
-      title: joi.string().required(),
+      title: joi.string().required().escapeHTML(),
       price: joi.number().required().min(0),
       // image: joi.string().required(),
-      location: joi.string().required(),
-      description: joi.string().required(),
+      location: joi.string().required().escapeHTML(),
+      description: joi.string().required().escapeHTML(),
     })
     .required(),
   deleteImages: joi.array(),
@@ -16,7 +40,7 @@ module.exports.parkingLotSchema = joi.object({
 module.exports.reviewSchema = joi.object({
   review: joi
     .object({
-      body: joi.string().required(),
+      body: joi.string().required().escapeHTML(),
       rating: joi.number().required().min(1).max(5),
     })
     .required(),
